b671cfc745f787ff3a189585dbf648c9b161ae6f
k-time <kenneth.xiao@gmail.com>
2017-03-18 18:18:02 -0500

    Updated structure.
    Removed old files.

--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BOT_HOME=$HOME/decision_bot
-
-echo 'Restarted bot at' `date` >> $BOT_HOME/log.txt
-$BOT_HOME/stop.sh
-$BOT_HOME/start-background.sh
--- start-foreground.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BOT_HOME=$HOME/decision_bot
-
-echo 'Started decision bot...'
-python3 $BOT_HOME/decision_bot.py -d
