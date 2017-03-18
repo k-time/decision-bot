68e75f09a93808b6729480566b9f5f545a2579d6
k-time <kenneth.xiao@gmail.com>
2017-03-18 16:04:59 -0400

    Notifying account when bot is starting up.

---
+++ notify_account.py
@@ -0,0 +1,24 @@
+import praw
+import yaml
+
+
+def main():
+    # Load configs
+    with open('config.yaml', 'r') as cfg_file:
+        cfg = yaml.load(cfg_file)
+
+    # Authentication
+    reddit = praw.Reddit(
+            client_id=cfg['client_id'],
+            client_secret=cfg['client_secret'],
+            user_agent=cfg['user_agent'],
+            username=cfg['username'],
+            password=cfg['pw'])
+
+    # Notify my account
+    reddit.redditor(cfg['personal_username']).message(
+        'DecisionBot starting', 'Attempting to start in background...')
+
+
+if __name__ == '__main__':
+    main()
--- start-background.sh
+++ start-background.sh
@@ -4,6 +4,7 @@ BOT_HOME=$HOME/decision_bot
 
 if ! pgrep -f 'decision_bot.py'
 then
+	nohup python3 $BOT_HOME/notify_account.py &
 	nohup python3 $BOT_HOME/decision_bot.py &
 	echo 'Started bot at' `date` >> $BOT_HOME/log.txt
 	echo '-------------' >> $BOT_HOME/log.txt
