a5421505f78ea057d950a5152aee032968823be7
k-time <kenneth.xiao@gmail.com>
2018-10-16 23:33:34 -0400

    Updating emoji.

--- decision_bot.py
+++ decision_bot.py
@@ -35,7 +35,7 @@ def build_comment_reply(score_tables, fight_result: str, media_scores: List[Tupl
     if "DANIEL CORMIER" in fight_result:
         fight_result = fight_result.replace("DANIEL CORMIER", "DANIEL CORMIER 🎂🍗", 1)
     if "ALEXANDER GUSTAFSSON" in fight_result:
-        fight_result = fight_result.replace("ALEXANDER GUSTAFSSON", "ALEXANDER GUSTAFSSON ⧨⧨", 1)
+        fight_result = fight_result.replace("ALEXANDER GUSTAFSSON", "ALEXANDER GUSTAFSSON ♠️⧨", 1)
     elif "ARTEM LOBOV" in fight_result:
         fight_result = fight_result.replace("ARTEM LOBOV", "THE GOAT", 1)
         if event_info is not None:
